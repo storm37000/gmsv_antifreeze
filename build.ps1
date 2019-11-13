7e9c0f2f51bd7af2fba1d26be2cdf9b91bf47d9d
storm37000 <6331913+storm37000@users.noreply.github.com>
2019-11-13 18:46:31 -0500

    fully ready to compile 64 bit

--- build.ps1
+++ build.ps1
@@ -1,16 +1,18 @@
 if($env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2019"){
-    $content = Get-Content -Path '.\src\main.cpp'
-    $newContent = $content -replace 'unsigned int build = 0', 'unsigned int build = ' + $env:APPVEYOR_BUILD_NUMBER
-    $newContent | Set-Content -Path '.\src\main.cpp'
-    svn checkout https://github.com/Facepunch/gmod-module-base/branches/development/include ..\include\
-    msbuild ".\projects\windows\gmsv_antifreeze.sln" /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	$content = Get-Content -Path '.\src\main.cpp'
+	$newContent = $content -replace 'unsigned int build = 0', 'unsigned int build = ' + $env:APPVEYOR_BUILD_NUMBER
+	$newContent | Set-Content -Path '.\src\main.cpp'
+	svn checkout https://github.com/Facepunch/gmod-module-base/branches/development/include ..\include\
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=Release /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+	msbuild ".\projects\windows\gmsv_antifreeze.sln" /property:Configuration=Release64 /verbosity:normal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 }else{
-    $content = Get-Content -Path './src/main.cpp'
-    $newContent = $content -replace 'unsigned int build = 0', 'unsigned int build = ' + $env:APPVEYOR_BUILD_NUMBER
-    $newContent | Set-Content -Path './src/main.cpp'
-    sudo apt-get update
-    sudo apt-get install gcc-multilib g++-multilib -y
-    svn checkout https://github.com/Facepunch/gmod-module-base/branches/development/include ../include/
-    cd ./projects/linux
-    make
+	$content = Get-Content -Path './src/main.cpp'
+	$newContent = $content -replace 'unsigned int build = 0', 'unsigned int build = ' + $env:APPVEYOR_BUILD_NUMBER
+	$newContent | Set-Content -Path './src/main.cpp'
+	sudo apt-get update
+	sudo apt-get install gcc-multilib g++-multilib -y
+	svn checkout https://github.com/Facepunch/gmod-module-base/branches/development/include ../include/
+	cd ./projects/linux
+	make config=release
+	make config=release64
 }
