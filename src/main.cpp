0e70a452bdfb1ac60baa6c130181b4ea40ce8639
storm37000 <6331913+storm37000@users.noreply.github.com>
2019-12-26 00:40:55 -0500

    optimizations, bugfixes, and general code cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -12,61 +12,61 @@ std::atomic<bool> flag (true);
 void foo() 
 {
 	unsigned short int timeout = 0;
-    while(flag){
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        if(srvrtime >= (std::time(nullptr))-2){
-    		if (timeout != 0){
+	while(flag){
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		if(srvrtime >= (std::time(nullptr))-2){
+			if (timeout != 0){
 				timeout = 0;
-    			std::cout << "Server caught back up!\n";
-    		}
-        }else{
-            if(srvrtime != 0){
-                timeout++;
-                std::cout << "Server is behind! (" << timeout << ")\n";
-                if(timeout == 60){
-                    std::cout << "Server Frozen! killing process...\n";
+				std::cout << "Server caught back up!\n";
+			}
+		}else{
+			if(srvrtime != 0){
+				timeout++;
+				std::cout << "Server is behind! (" << timeout << ")\n";
+				if(timeout == 60){
+					std::cout << "Server Frozen! killing process...\n";
 					throw std::exception();
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 }
 std::thread t1(foo);
 LUA_FUNCTION( WatchDogPing )
 {
 	srvrtime = std::time(nullptr);
-    return 0;
+	return 0;
 }
 LUA_FUNCTION( WatchDogStop )
 {
 	flag = false;
 	t1.join();
-    return 0;
+	return 0;
 }
 GMOD_MODULE_OPEN()
 {
-    LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB );// Push global table
-    LUA->PushString( "WatchDogPing" );
-    LUA->PushCFunction(WatchDogPing);
-    LUA->SetTable( -3 );
-    LUA->PushString( "WatchDogStop" );
-    LUA->PushCFunction(WatchDogStop);
-    LUA->SetTable( -3 );
-	LUA->PushString( "antifreeze" );
+	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB );// Push global table
 	LUA->CreateTable();
-	LUA->PushString( "version" );
-	LUA->PushNumber( 1 );
-	LUA->SetTable( -3 );
-	LUA->PushString( "build" );
-	LUA->PushNumber( build );
-	LUA->SetTable( -3 );
-	LUA->SetTable( -3 );
+	LUA->PushInteger( 1 );
+	LUA->SetField( -2, "version" );
+	LUA->PushInteger( build );
+	LUA->SetField( -2, "build" );
+	LUA->PushCFunction(WatchDogStop);
+	LUA->SetField( -2, "WatchdogStop" );
+	LUA->SetField( -2, "antifreeze" );
+	LUA->GetField(-1, "timer");
+	LUA->GetField(-1, "Create");
+	LUA->PushString("antifreeze_watchdog");
+	LUA->PushInteger(1);
+	LUA->PushInteger(0);
+	LUA->PushCFunction(WatchDogPing);
+	LUA->Call(4, 0);
 	LUA->Pop(1);
-    return 0;
+	return 0;
 }
 GMOD_MODULE_CLOSE()
 {
 	flag = false;
 	t1.join();
-    return 0;
+	return 0;
 }
